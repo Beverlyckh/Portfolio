30c8e3360b2a22069159943514e490eb98b11781
Beverly_ckh <beverly99@hotmail.fr>
2018-12-14 21:57:25 -0800

    Updates

---
+++ precache-manifest.8b0a91384c322c469cbe6c228c0df5ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee2a100325fd284f0097",
+    "url": "/portfolio/static/js/runtime~main.ee2a1003.js"
+  },
+  {
+    "revision": "c7537f33674b7176c9e6",
+    "url": "/portfolio/static/js/main.c7537f33.chunk.js"
+  },
+  {
+    "revision": "c5e39860dbe3f2df43f8",
+    "url": "/portfolio/static/js/1.c5e39860.chunk.js"
+  },
+  {
+    "revision": "c7537f33674b7176c9e6",
+    "url": "/portfolio/static/css/main.2aa27267.chunk.css"
+  },
+  {
+    "revision": "c5e39860dbe3f2df43f8",
+    "url": "/portfolio/static/css/1.5e2fd8f5.chunk.css"
+  },
+  {
+    "revision": "29fb1debdb96a8a2f3edfe36273dc62d",
+    "url": "/portfolio/index.html"
+  }
+];
